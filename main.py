3db1ef4cdfbfb0b5a60b53d4aa600b9ef41db8df
TIGER <dhdbfrbs12345@gmail.com>
2024-01-27 00:27:09 +0900

    basic code finished

--- main.py
+++ main.py
@@ -4,13 +4,29 @@ player = 0
 computer = 0
 num_to_str = ""
 num_to_str_for_computer = ""
+answer = ""
 running = True
 
+def reset():
+    global player
+    global computer
+    global num_to_str
+    global num_to_str_for_computer
+    global answer
+    global running
+    player = 0
+    computer = 0
+    num_to_str = ""
+    num_to_str_for_computer = ""
+    answer = ""
+    running = True
+
 while running:
-    player = int(input("가위:1  바위:2  보:3"))
+    player = int(input("가위:1  바위:2  보:3\n\n"))
+    print("\n")
 
     if player < 1 or player > 3:
-        print("error")
+        print("error\n\n")
 
     computer = random.randrange(1, 4)
 
@@ -35,7 +51,7 @@ while running:
     if player == computer:
         print("나: %s" % num_to_str)
         print("상대방: %s" % num_to_str_for_computer)
-        print("비김")
+        print("비김\n\n")
 
 
 
@@ -43,10 +59,44 @@ while running:
         if computer == 2:
             print("나: %s" % num_to_str)
             print("상대방: %s" % num_to_str_for_computer)
-            print("짐")
+            print("짐\n\n")
 
         else:
             print("나: %s" % num_to_str)
             print("상대방: %s" % num_to_str_for_computer)
-            print("이김")
+            print("이김\n\n")
+
+
+    if player == 2:
+        if computer == 3:
+            print("나: %s" % num_to_str)
+            print("상대방: %s" % num_to_str_for_computer)
+            print("짐\n\n")
+
+        else:
+            print("나: %s" % num_to_str)
+            print("상대방: %s" % num_to_str_for_computer)
+            print("이김\n\n")
+
+
+
+    if player == 3:
+        if computer == 1:
+            print("나: %s" % num_to_str)
+            print("상대방: %s" % num_to_str_for_computer)
+            print("짐\n\n")
+
+        else:
+            print("나: %s" % num_to_str)
+            print("상대방: %s" % num_to_str_for_computer)
+            print("이김\n\n")
+
+    answer = input("again:a or quit for any key exept a\n\n")
+
+    if answer == "a":
+        reset()
+        print("\n\n")
+
+    else:
+        running = False
 
